407f8d622b73a7261cbf0046e2ceeaffef5615dd
Al Clark <alanjclark@gmail.com>
2022-01-14 17:36:46 +0000

    Reverts

--- aloha/main.go
+++ aloha/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"time"
 )
 
 func main() {
-	pwd, _ := os.Getwd()
-	host, _ := os.Hostname()
+	// pwd, _ := os.Getwd()
+	// host, _ := os.Hostname()
 	// log.Infow("Hello Go World", "hostname", host, "work_dir", pwd)
 	fmt.Printf("🌋: Hello World!\n⏰: %s\n", time.Now().Format("2006-01-02T15:04:05-0700"))
 
