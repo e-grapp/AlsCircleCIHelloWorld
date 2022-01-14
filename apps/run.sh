869b35579cbc4cf06b2dede1bef3b5f41a9bf5e4
Al Clark <alanjclark@gmail.com>
2022-01-14 12:12:39 +0000

    adding set -o xtrace

--- apps/run.sh
+++ apps/run.sh
@@ -1,4 +1,6 @@
-./apps/aloha
-./apps/ksleep 2 1.5
-./apps/ksshgen -a rsa --override -b 4096
-./apps/kpwdgen -l 16 -s 0 -c 1
+set -o xtrace
+
+./aloha
+./ksleep 2 1.5
+./ksshgen -a rsa --override -b 4096
+./kpwdgen -l 16 -s 0 -c 1
