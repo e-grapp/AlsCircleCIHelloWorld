c7df42cfa5dc6ed7e6a3218d968a2e27204772f1
Al Clark <alanjclark@gmail.com>
2022-01-14 13:09:29 +0000

    Running go with go

--- aloha/main.go
+++ aloha/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
-	// pwd, _ := os.Getwd()
-	// host, _ := os.Hostname()
-	// log.Infow("Hello Go World", "hostname", host, "work_dir", pwd)
+	pwd, _ := os.Getwd()
+	host, _ := os.Hostname()
+	log.Infow("Hello Go World", "hostname", host, "work_dir", pwd)
 	fmt.Printf("🌋: Hello World!\n⏰: %s\n", time.Now().Format("2006-01-02T15:04:05-0700"))
 
 	fmt.Println(`	┌─┐┬  ┌─┐┬ ┬┌─┐
