ce82214daac406acd7054a29c6afc787957893fc
Al Clark <alanjclark@gmail.com>
2022-01-14 12:58:22 +0000

    Checking shell

--- apps/run.sh
+++ apps/run.sh
@@ -1,12 +1,16 @@
 set -o xtrace
 
+ps
 go run ./aloha
-go run ./aloha
-go run ./aloha
-go run ./aloha
-./aloha
-./aloha
-./aloha
-./ksleep 2 1.5
-# ./ksshgen -a rsa --override -b 4096
-./kpwdgen -l 16 -s 0 -c 1
+ps
+sleep 5s
+ps
+# go run ./aloha
+# go run ./aloha
+# go run ./aloha
+# ./aloha
+# ./aloha
+# ./aloha
+# ./ksleep 2 1.5
+# # ./ksshgen -a rsa --override -b 4096
+# ./kpwdgen -l 16 -s 0 -c 1
